07f8ac344cb4858d36c20cca59bf33b8da18b182
Hung FPT <hungtrong@Hungs-MacBook-Pro.local>
2013-06-18 00:47:46 +0700

    Setting version 1.0

---
+++ AbcProject/AbcProjectTests/AbcProjectTests.m
@@ -0,0 +1,36 @@
+//
+//  AbcProjectTests.m
+//  AbcProjectTests
+//
+//  Created by Hung FPT on 6/18/13.
+//  Copyright (c) 2013 Hung FPT. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AbcProjectTests : XCTestCase
+
+@end
+
+@implementation AbcProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
